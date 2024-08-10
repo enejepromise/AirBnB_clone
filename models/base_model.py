07a123b5588dab2c69d169756dfdcf77a9150330
eneje promise <enejepromise@gmail.com>
2024-08-10 05:08:32 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
-
-class BaseModel:
-    """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+class BaseModel(self):
+    """This is a basemodel that defines all the instances of a common element"""
+    def __ini__(self):
+        self.id = str(uuid4)
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+   
+   def __str__(self):
+        """Return a string representation of the instance."""
+        return f"[{self.__class__. __name__}] (self.id) {self.__dict__}"
+    
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.utcnow()
+    
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = [self.__class__.__name__]
+        my_dict["self.created_at"] = datetime.utcnow()
+        my_dict["self.updated_at"] = datetime.utcnow()
+        return my_dict
