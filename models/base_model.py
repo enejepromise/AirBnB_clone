b42c278d013066de1f2146a98b977be299d56f7e
eneje promise <enejepromise@gmail.com>
2024-09-01 05:29:41 -0700

    updated kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,17 +7,27 @@ class BaseModel:
     """Class that will common attributes/method will inherit from"""
     format = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __init__(self):
+    def __init__(self, *_, **kwargs):
         """This a method that initailizes"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = deepcopy(self.created_at)
+        if len(kwargs)!= 0:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    if k == "created_at" or k == "updated_at":
+                setattr(self, k, datetime.strftime(v, BaseModel.format))
+            else:
+                setattr(self, k,v)
+            else:
+
+                self.id = str(uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = deepcopy(self.created_at)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         dct = {}
+
         dct.update(self.__dict__)
         dct["__class__"] = self.__class__.__name__
         dct["created_at"] = self.created_at.strftime(self.format)
