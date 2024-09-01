1e723bd2b9498f6e4cec7e433ee5d64fd5bcc5ab
eneje promise <enejepromise@gmail.com>
2024-09-01 04:29:42 -0700

    This is the base model where other classes will inherit from

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,29 @@
-#!/usr/bin/env python3
-"""
-This module contains BaseModel implementation
-"""
-import uuid
-from models import storage
+#!/usr/bin/python3
 from datetime import datetime
-
+from uuid import uuid4
+from copy import deepcopy
 
 class BaseModel:
-    """Represents a BaseModel with basic attributes/methods"""
-    def __init__(self, *args, **kwargs):
-        """Initializes BaseModel instances"""
-        if kwargs:
-            exed = ["created_at", "updated_at"]
+    """Class that will common attributes/method will inherit from"""
+    format = %Y-%m-%dT%H:%M:%S.%f
 
-            for key, value in kwargs.items():
-                if key in exed and isinstance(value, str):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.updated_at = self.created_at = datetime.now()
-            storage.new(self)
-
-    def __str__(self):
-        """Returns formatted string of the instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
+    def __init__(self):
+        """This a method that initailizes"""
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = deepcopy(self.created_at)
     def save(self):
-        """Updates the `updated_at` timestamp"""
         self.updated_at = datetime.now()
 
-        # Updates the timestamp on saved instance before saving to file
-        storage.new(self)
-        storage.save()
-
     def to_dict(self):
-        """Returns dictionary representation of BaseModel instance"""
-        re_dict = self.__dict__.copy()
-        re_dict["created_at"] = self.created_at.isoformat()
-        re_dict["updated_at"] = self.updated_at.isoformat()
-        re_dict["__class__"] = self.__class__.__name__
+        dct = {}
+        dct.update(self.__dict__)
+        dct["__class__"] = self.__class__.__name__
+        dct["created_at"] = self.datetime.strftime(BaseModel.format)
+        dct["updated_at"] = self.datetime.strftime(BaseModel.format)
 
-        return re_dict
+        return dct
+
+    def __str__(self) -> str:
+        return f"[{self.__class__. __name__}] ({self.id}) {self.__dict__}"
+
